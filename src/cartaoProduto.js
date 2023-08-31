fccc6f458634984348e84a5d3031b2ed6472ff1e
Eledilson B. Ditore <52582568+HipCoin3@users.noreply.github.com>
2023-08-30 22:01:59 -0300

    Estilizando catalogo de produtos e add produtos ao carrinho

---
+++ src/cartaoProduto.js
@@ -0,0 +1,46 @@
+import { catalogo } from "./utils/utilidade";
+import { addAoCarrinho } from "./menuCarrinho";
+
+
+export function catalogoProdutos() {
+
+    for (const produtoCatalogo of catalogo) {
+
+        const cartaoProduto = `
+        <div 
+        class=
+        'border-solid shadow-lg
+        shadow-cyan-900
+        w-48 my-1.5 mx-1.5 flex 
+        flex-col p-2 justify-between 
+        group rounded-lg' 
+         
+         id="product-card-${catalogo.id}"
+         >
+        <img
+        src="./assets/img/${produtoCatalogo.imagem}"
+        alt="Produto 1 do MagazineHash"
+        class="group-hover:scale-110 duration-300 my-2 shadow-sm rounded-lg"
+        >
+        <p class="text-sm">${produtoCatalogo.marca}</p>
+        <p class="text-sm">${produtoCatalogo.nome}</p>
+        <p class="text-sm">R$ ${produtoCatalogo.preco}</p>
+        <button 
+        id="adicionar-${produtoCatalogo.id}"
+        class=
+        "bg-cyan-800 text-cyan-50 
+        rounded-lg w-25 hover:bg-cyan-950
+        duration-300 justify-center"
+        >
+        <i class="fa-solid fa-cart-plus"></i>
+        </button>
+        </div>
+`
+        document.getElementById('product-container').innerHTML += cartaoProduto;
+    };
+
+    for (const produtoCatalogo of catalogo) {
+        document.getElementById(`adicionar-${produtoCatalogo.id}`).addEventListener('click', () => addAoCarrinho(produtoCatalogo.id));
+
+    }
+}
--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,3 +1,6 @@
+import { catalogoProdutos } from "./cartaoProduto";
+import { catalogo } from "./utils/utilidade";
+
 function abrirCarrinho() {
 
     document.getElementById('carrinho').classList.remove('right-[-380px]');
@@ -21,4 +24,35 @@ export function inicializarCarrinho() {
     botaoFecharCarrinho.addEventListener('click', fecharCarrinho);
 
 };
+
+export function addAoCarrinho(idPorduto) {
+
+    const produto = catalogo.find((p) => p.id === idPorduto);
+
+    const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+
+    const produtoSelecionado = `
+    <article class="flex text-xs relative bg-cyan-900 shadow-inner shadow-cyan-950 rounded-lg ">
+    
+      <img src="./assets/img/${produto.imagem}" alt="carrinho: ${produto.nome}"
+      class="h-20 rounded-lg "
+      >
+      <div class="p-2">
+        <p class="text-slate-50 text-sm">${produto.nome}</p>
+        <p class="text-slate-400 text-xs">Tamanho: ${produto.tamanho}</p>
+        <p class="text-black text-[1.5em]">R$${produto.preco}</p>
+      </div>
+
+      <button 
+      id="remover-produto"
+      class=" ml-12 absolute top-0 right-0 m-3 hover:text-black hover:duration-100 text-[#a1c5f7c2]"
+      >
+        <i class="fa-solid fa-circle-xmark"></i>
+      </button>
+    </article>
+    `;
+
+    containerProdutosCarrinho.innerHTML += produtoSelecionado;
+};
+
 
