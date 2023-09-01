fee26a158937ad6fdb291fbbcd45e086a12a9e8c
Eledilson B. Ditore <52582568+HipCoin3@users.noreply.github.com>
2023-08-31 22:45:31 -0300

    Incrementando, decrementando e removendo produtos do carrinho

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,58 +1,159 @@
-import { catalogoProdutos } from "./cartaoProduto";
 import { catalogo } from "./utils/utilidade";
+
+// Um dicionário que "busca"/manipula os produtos pelo ID.
+const idsProdutosCarrinhoComQuantidade = {};
 
 function abrirCarrinho() {
 
-    document.getElementById('carrinho').classList.remove('right-[-380px]');
-    document.getElementById('carrinho').classList.add('right-0');
+  document.getElementById('carrinho').classList.remove('right-[-380px]');
+  document.getElementById('carrinho').classList.add('right-0');
 
 };
 
 function fecharCarrinho() {
 
-    document.getElementById('carrinho').classList.remove('right-0');
-    document.getElementById('carrinho').classList.add('right-[-380px]');
+  document.getElementById('carrinho').classList.remove('right-0');
+  document.getElementById('carrinho').classList.add('right-[-380px]');
 
 };
 
 export function inicializarCarrinho() {
 
-    const botaoFecharCarrinho = document.getElementById('fechar-carrinho');
-    const botaoAbrirCarrinho = document.getElementById('abrir-carrinho');
+  const botaoFecharCarrinho = document.getElementById('fechar-carrinho');
+  const botaoAbrirCarrinho = document.getElementById('abrir-carrinho');
 
-    botaoAbrirCarrinho.addEventListener('click', abrirCarrinho);
-    botaoFecharCarrinho.addEventListener('click', fecharCarrinho);
+  botaoAbrirCarrinho.addEventListener('click', abrirCarrinho);
+  botaoFecharCarrinho.addEventListener('click', fecharCarrinho);
 
 };
 
-export function addAoCarrinho(idPorduto) {
+function removerDoCarrinho(idPorduto) {
+  delete idsProdutosCarrinhoComQuantidade[idPorduto];
+  renderizarProdutosCarrinho();
+};
 
-    const produto = catalogo.find((p) => p.id === idPorduto);
 
-    const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+function incrementarQuantidadeProduto(idPorduto) {
+  idsProdutosCarrinhoComQuantidade[idPorduto]++;
+  atualizarInfoQuantidade(idPorduto);
+};
 
-    const produtoSelecionado = `
-    <article class="flex text-xs relative bg-cyan-900 shadow-inner shadow-cyan-950 rounded-lg ">
-    
-      <img src="./assets/img/${produto.imagem}" alt="carrinho: ${produto.nome}"
-      class="h-20 rounded-lg "
-      >
-      <div class="p-2">
-        <p class="text-slate-50 text-sm">${produto.nome}</p>
-        <p class="text-slate-400 text-xs">Tamanho: ${produto.tamanho}</p>
-        <p class="text-black text-[1.5em]">R$${produto.preco}</p>
-      </div>
+function decrementarQuantidadeProduto(idPorduto) {
 
+  if (idsProdutosCarrinhoComQuantidade[idPorduto] === 1) {
+    removerDoCarrinho(idPorduto);
+    return;
+  }
+
+  idsProdutosCarrinhoComQuantidade[idPorduto]--;
+  atualizarInfoQuantidade(idPorduto);
+
+};
+
+function atualizarInfoQuantidade(idPorduto) {
+  document.getElementById(`quantidade-${idPorduto}`).innerText =
+    idsProdutosCarrinhoComQuantidade[idPorduto];
+};
+
+function desenharProdutoNoCarrinho(idPorduto) {
+  const produto = catalogo.find((p) => p.id === idPorduto);
+
+  const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+
+  // Aqui estou criando um elemento article para substituir o article do texto abaixo.
+  const elementoArticle = document.createElement("article");
+  const articleClasses = [
+    "flex",
+    "text-xs",
+    "relative",
+    "bg-cyan-900",
+    "shadow-inner",
+    "shadow-cyan-950",
+    "rounded-lg"
+  ];
+
+  for (const articleClass of articleClasses) {
+    elementoArticle.classList.add(articleClass);
+  }
+
+  // return => <article class="flex text-xs relative bg-cyan-900 shadow-inner shadow-cyan-950 rounded-lg "></article>
+
+  const produtoSelecionado = `
+      
       <button 
-      id="remover-produto"
+      id="remover-produto-${produto.id}"
       class=" ml-12 absolute top-0 right-0 m-3 hover:text-black hover:duration-100 text-[#a1c5f7c2]"
       >
         <i class="fa-solid fa-circle-xmark"></i>
       </button>
-    </article>
+
+      <img src="./assets/img/${produto.imagem}" alt="carrinho: ${produto.nome}"
+      class="h-20 rounded-lg "
+      >
+      <div class="p-2 flex flex-col justify-between">
+        
+      <p class="text-slate-50 text-sm">
+        ${produto.nome}
+        </p>
+        
+        <p class="text-slate-400 text-xs">
+        Tamanho: ${produto.tamanho}
+        </p>
+
+        <p class="text-black text-[1.5em]">
+        R$${produto.preco}
+        </p>
+
+      </div>
+
+      <div class="flex flex-row text-slate-100 items-end absolute bottom-1 right-2 text-lg">
+      
+        <button id="decrementar-produto-${produto.id}">-</button>
+        <p id="quantidade-${produto.id}" class="ml-2">${idsProdutosCarrinhoComQuantidade[produto.id]}</p>
+        <button id="incrementar-produto-${produto.id}" class="ml-2">+</button>
+      
+      </div>
+  
     `;
 
-    containerProdutosCarrinho.innerHTML += produtoSelecionado;
+  // Aqui eu estou jogando os elementos de testo da variável produtoSelecionado dentro do elemento article criado no JS.
+  elementoArticle.innerHTML = produtoSelecionado;
+  containerProdutosCarrinho.appendChild(elementoArticle);
+
+  document.getElementById(`incrementar-produto-${produto.id}`)
+    .addEventListener('click', () => incrementarQuantidadeProduto(produto.id));
+
+  document.getElementById(`decrementar-produto-${produto.id}`)
+    .addEventListener('click', () => decrementarQuantidadeProduto(produto.id));
+
+  document.getElementById(`remover-produto-${produto.id}`)
+    .addEventListener('click', () => removerDoCarrinho(produto.id));
+
+};
+
+function renderizarProdutosCarrinho() {
+  const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+  containerProdutosCarrinho.innerHTML = "";
+
+  for (const idPorduto in idsProdutosCarrinhoComQuantidade) {
+    desenharProdutoNoCarrinho(idPorduto);
+  };
+
+};
+
+
+export function addAoCarrinho(idPorduto) {
+
+  // mapeia os ids dentro do carrinho e se já existir, não add outro card.
+  if (idPorduto in idsProdutosCarrinhoComQuantidade) {
+
+    incrementarQuantidadeProduto(idPorduto);
+    return;
+  }
+  //adiciona um produto ao carrinho pelo ID.
+  idsProdutosCarrinhoComQuantidade[idPorduto] = 1;
+
+  desenharProdutoNoCarrinho(idPorduto);
 };
 
 
--- src/utils/utilidade.js
+++ src/utils/utilidade.js
@@ -1,75 +1,75 @@
 
 export const catalogo = [
     {
-        id: 1,
+        id: "1",
         marca: "Zara",
         nome: "Camisa Larga com Bolsos",
         preco: 70,
         imagem: "product-1.jpg",
         feminino: false,
-        tamanho: "P,M,G,GG"
+        tamanho: "M"
     },
     {
-        id: 2,
+        id: "2",
         marca: "Zara",
         nome: "Casaco Reto com Lã",
         preco: 85,
         imagem: "product-2.jpg",
         feminino: true,
-        tamanho: "P,M,G,GG"
+        tamanho: "GG"
     },
     {
-        id: 3,
+        id: "3",
         marca: "Zara",
         nome: "Jaqueta com Efeito Camurça",
         preco: 60,
         imagem: "product-3.jpg",
         feminino: false,
-        tamanho: "P,M,G,GG"
+        tamanho: "G"
     },
     {
-        id: 4,
+        id: "4",
         marca: "Zara",
         nome: "Sobretudo em Mescla de Lã",
         preco: 160,
         imagem: "product-4.jpg",
         feminino: false,
-        tamanho: "P,M,G,GG"
+        tamanho: "P"
     },
     {
-        id: 5,
+        id: "5",
         marca: "Zara",
         nome: "Camisa Larga Acolchoada de Veludo Cotelê",
         preco: 110,
         imagem: "product-5.jpg",
         feminino: false,
-        tamanho: "P,M,G,GG"
+        tamanho: "M"
     },
     {
-        id: 6,
+        id: "6",
         marca: "Zara",
         nome: "Casaco de Lã com Botões",
         preco: 170,
         imagem: "product-6.jpg",
         feminino: true,
-        tamanho: "P,M,G,GG"
+        tamanho: "GG"
     },
     {
-        id: 7,
+        id: "7",
         marca: "Zara",
         nome: "Casaco com Botões",
         preco: 75,
         imagem: "product-7.jpg",
         feminino: true,
-        tamanho: "P,M,G,GG"
+        tamanho: "G"
     },
     {
-        id: 8,
+        id: "8",
         marca: "Zara",
         nome: "Colete Comprido com Cinto",
         preco: 88,
         imagem: "product-8.jpg",
         feminino: true,
-        tamanho: "P,M,G,GG"
+        tamanho: "M"
     },
 ];
